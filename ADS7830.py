917fe640cc86749c5b0d6cdb1c41f8bcb60112c8
Stepan Bechynsky <stepan@bechynsky.cz>
2015-12-26 17:53:27 +0000

    Added comments

--- ADS7830.py
+++ ADS7830.py
@@ -1,12 +1,17 @@
 import smbus
 
+# Datashet for TI ADC ADS7830
 # http://www.ti.com/lit/ds/symlink/ads7830.pdf
 class ADS7830:
+    # busid - 1 for new Raspberry Pi, 0 for old versions
+    # address - I2C address of ADS7830
     def __init__(self, busid, address):
         self.bus = smbus.SMBus(busid)
         self.address = address
 
+    # channel - ADC channel 0-7
     def Read(self, channel):
+        # see page 13 and 14 of datasheet
         channelAddress = 0b10000100 | ((channel / 2) if (channel % 2 == 0) else ((channel / 2) + 4)) << 4
 	self.bus.write_byte(self.address, channelAddress)
         adcValue = self.bus.read_byte(self.address)
