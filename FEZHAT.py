66c920b45319fa2af33923dc1fd3132ac52e9ed3
Štěpán Bechynský <stepan@bechynsky.cz>
2015-12-27 15:40:04 +0100

    New read_analog and analog pin constants

--- FEZHAT.py
+++ FEZHAT.py
@@ -1,6 +1,11 @@
 import ADS7830
 
 class FEZHAT:
+    AIN1 = 1
+    AIN2 = 2
+    AIN3 = 3
+    AIN6 = 6
+    AIN7 = 7
     
     def __init__(self):
         self._ads = ADS7830.ADS7830(1, 0x48)
@@ -13,8 +18,11 @@ class FEZHAT:
         # see page 8
         return (((3300 / 255) * self._ads.read(4)) - 400) / 19.5
 
+    def read_analog(channel):
+        return self._ads.read(channel)
 
 if __name__ == "__main__":
     fh = FEZHAT()
     print fh.get_light()
     print fh.get_temperature()
+    print fh.read_analog(FEZHAT.AIN1)
