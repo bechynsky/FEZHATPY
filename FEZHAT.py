f5295b04315f44866e2d5765d1e0795c3d16830c
Štěpán Bechynský <stepan@bechynsky.cz>
2015-12-27 11:48:29 +0100

    First commit, get light and temperature functions

---
+++ FEZHAT.py
@@ -0,0 +1,14 @@
+import ADS7830
+
+class FEZHAT:
+    
+    def __init__(self):
+        self._ads = ADS7830(1, 0x48)
+        
+    def get_light():
+        return self._ads.read(5) / 255.0
+    
+    # http://ww1.microchip.com/downloads/en/DeviceDoc/20001942F.pdf
+    def get_temperature():
+        # see page 8
+        return (((3.3 / 255) * self._ads.read(4)) - 400) / 19.5
