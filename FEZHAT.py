736c80cc0cca2cc898f43f29febbfd7fa0f93c27
Štěpán Bechynský <stepan@bechynsky.cz>
2015-12-27 11:50:06 +0100

    Added test code

--- FEZHAT.py
+++ FEZHAT.py
@@ -3,7 +3,7 @@ import ADS7830
 class FEZHAT:
     
     def __init__(self):
-        self._ads = ADS7830(1, 0x48)
+        self._ads = ADS7830.ADS7830(1, 0x48)
         
     def get_light():
         return self._ads.read(5) / 255.0
@@ -12,3 +12,9 @@ class FEZHAT:
     def get_temperature():
         # see page 8
         return (((3.3 / 255) * self._ads.read(4)) - 400) / 19.5
+
+
+if __name__ == "__main__":
+    fh = FEZHAT()
+    print fh.get_light()
+    print fh.get_temperature()
