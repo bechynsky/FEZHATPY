ef28aa57d5ecdaea4eb4eeaa0cbd14a6a8ae29da
Štěpán Bechynský <stepan@bechynsky.cz>
2015-12-27 11:48:08 +0100

    Variable and function name clenup

--- ADS7830.py
+++ ADS7830.py
@@ -6,19 +6,19 @@ class ADS7830:
     # busid - 1 for new Raspberry Pi, 0 for old versions
     # address - I2C address of ADS7830
     def __init__(self, busid, address):
-        self.bus = smbus.SMBus(busid)
-        self.address = address
+        self._bus = smbus.SMBus(busid)
+        self._address = address
 
     # channel - ADC channel 0-7
-    def Read(self, channel):
+    def read(self, channel):
         # see page 13 and 14 of datasheet
         channelAddress = 0b10000100 | ((channel / 2) if (channel % 2 == 0) else ((channel / 2) + 4)) << 4
-	self.bus.write_byte(self.address, channelAddress)
-        adcValue = self.bus.read_byte(self.address)
+        self._bus.write_byte(self._address, channelAddress)
+        adcValue = self._bus.read_byte(self._address)
         return adcValue
 
 
 if __name__ == "__main__":
     ads = ADS7830(1, 0x48)
     for i in range(0,8):
-        print "Channel {0}: {1}".format(i, ads.Read(i))
+        print "Channel {0}: {1}".format(i, ads.read(i))
